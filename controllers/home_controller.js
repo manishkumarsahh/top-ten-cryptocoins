ec54c6c03bc7681f2a5ad096c1a791601696a400
Manish kumar sah <manishsah23121999@gmail.com>
2021-10-17 01:13:05 +0530

    routes added

---
+++ controllers/home_controller.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const Coin = require("../models/coins");
+const axios = require('axios');
+async function  sendToDb(req,res) {
+    try {
+      const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
+      let fetchedData = response.data;
+      let stringData = JSON.stringify(fetchedData);
+      const pushData = await Coin.findByIdAndUpdate({_id:'616b0149a325c444ea4e1e2a'},{allcoins:stringData});
+      return fetchedData;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  async function getFromDb(req,res) {
+    try {
+      
+      const pushData = await Coin.find({});
+      return pushData;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+module.exports.fetch =async function(req, res){
+    
+    sendToDb();
+    const data = await getFromDb();
+    const myJSON = JSON.stringify(data);
+    const myArr = myJSON.split("}");
+    let firstTen = [];
+    for(let i=0;i<10;i++){
+      myArr[i].replace(/\/$/, '');
+      
+      firstTen.push(myArr[i]);
+    }
+
+   res.send(firstTen);
+
+}
+
+module.exports.home = function(req,res){
+    res.sendFile(path.join(__dirname + '/../index.html'));
+
+}
--- index.js
+++ index.js
@@ -8,49 +8,53 @@ const axios = require('axios');
 app.use(express.urlencoded({extended:true}));
 
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-})
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// })
 
 
-async function sendToDb(req,res) {
-    try {
-      const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
-      let fetchedData = response.data;
-      let stringData = JSON.stringify(fetchedData);
-      const pushData = await Coin.findByIdAndUpdate({_id:'616b0149a325c444ea4e1e2a'},{allcoins:stringData});
-      return fetchedData;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+// async function sendToDb(req,res) {
+//     try {
+//       const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
+//       let fetchedData = response.data;
+//       let stringData = JSON.stringify(fetchedData);
+//       const pushData = await Coin.findByIdAndUpdate({_id:'616b0149a325c444ea4e1e2a'},{allcoins:stringData});
+//       return fetchedData;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
 
 
-  async function getFromDb(req,res) {
-    try {
+//   async function getFromDb(req,res) {
+//     try {
       
-      const pushData = await Coin.find({});
-      return pushData;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+//       const pushData = await Coin.find({});
+//       return pushData;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
 
-app.get('/fetch', async function(req,res){
-    sendToDb();
-    const data = await getFromDb();
-    const myJSON = JSON.stringify(data);
-    const myArr = myJSON.split("}");
-    let firstTen = [];
-    for(let i=0;i<10;i++){
-      myArr[i].replace(/\/$/, '');
+  
+
+// app.get('/fetch', async function(req,res){
+//     sendToDb();
+//     const data = await getFromDb();
+//     const myJSON = JSON.stringify(data);
+//     const myArr = myJSON.split("}");
+//     let firstTen = [];
+//     for(let i=0;i<10;i++){
+//       myArr[i].replace(/\/$/, '');
       
-      firstTen.push(myArr[i]);
-    }
+//       firstTen.push(myArr[i]);
+//     }
 
-   res.send(firstTen);
-});
+//    res.send(firstTen);
+// });
 
+
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
